c8aa26f7ed84423a0ea5cc0ff415a37e45054a41
Sammy Jackson <sam.jackson@live.ca>
2021-02-22 22:17:55 -0500

    adding code from three js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame } from 'react-three-fiber'
+import './css/styles.css'
+import Main from './components/Main';
+import { getMousePos } from "./javascript/utils" 
 
 function App() {
+  const mousePos = useRef({ x: 0, y: 0 })
+  const mouse = useRef([0, 0])
+
+  const onMouseMove = useCallback((e) => {
+    mouse.current = [e.clientX - window.innerWidth / 2, e.clientY - window.innerHeight / 2];
+    mousePos.current = getMousePos(e);
+    //console.log(mousePos.current);
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      {/* <div id="info">Description</div> */}
+      <Canvas
+      onMouseMove={onMouseMove}
+      gl={{ antialias: false, alpha: false }}
+      camera={{ position: [0, 0, 15], near: 5, far: 20 }}
+      onCreated={({ gl }) => gl.setClearColor('black')}>
+        <ambientLight />
+        <pointLight position={[150, 150, 150]} intensity={0.55} />
+        <Main mouse={mouse}/>
+      </Canvas>
     </div>
   );
 }
