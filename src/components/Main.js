8c0eab44fcddc56963cc5d9f889ed8729083f634
Sammy Jackson <sam.jackson@live.ca>
2021-02-28 17:22:34 -0500

    added changing position based on linked nodes

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,29 +12,35 @@ const Main = ({mouse}) => {
     var createGraph = require('ngraph.graph');
     var g = createGraph();
 
-    g.addNode('a', { scene: <JackyObject />, pos: [0,5,1]})
-    g.addNode('b', { scene: <JackyObject />, pos: [0,-5,2]})
-    g.addNode('c', { scene: <JackyObject />, pos: [3,0,3]})
-    g.addNode('d', { scene: <JackyObject />, pos: [-3,0,-3]})
+    // g.addNode('a', { scene: <JackyObject />, pos: [0,5,1]})
+    // g.addNode('b', { scene: <JackyObject />, pos: [0,-5,2]})
+    // g.addNode('c', { scene: <JackyObject />, pos: [3,0,3]})
+    // g.addNode('d', { scene: <JackyObject />, pos: [-3,0,-3]})
+    g.addNode('a', { scene: <JackyObject />, pos: [0,0,30]})
+    g.addNode('b', { scene: <JackyObject />, pos: [0,0,30]})
+    g.addNode('c', { scene: <JackyObject />, pos: [0,0,30]})
+    g.addNode('d', { scene: <JackyObject />, pos: [0,0,30]})
 
     g.addLink('a', 'b')
     g.addLink('a', 'c')
+    g.addLink('a', 'd')
+    g.addLink('b', 'd')
 
     const onClick = (clickedNode) => {
-        g.forEachNode((node) => {
-            //set each node to the original position and center the clicked node
-            node.id == clickedNode ? positions.current.get(node.id).current = [0,0,0] : positions.current.get(node.id).current = node.data.pos
-        })
-        //change the positions of the linked nodes to be around the clicked node
-        g.forEachLinkedNode(clickedNode, (linkedNode) => {
-
-            console.log("Connected node: ", linkedNode.id, linkedNode.data)
-        })
-        setActiveNode(clickedNode)
-    }
-    
-    const findPosition = (node, pos) => {
-        return null
+        if(clickedNode != activeNode){
+            g.forEachNode((node) => {
+                //set each node to the original position and center the clicked node
+                node.id == clickedNode ? positions.current.get(node.id).current = [0,0,0] : positions.current.get(node.id).current = node.data.pos
+            })
+            //change the positions of the linked nodes to be around the clicked node
+            let i = 0
+            g.forEachLinkedNode(clickedNode, (linkedNode) => {
+                positions.current.get(linkedNode.id).current = [i,5,i]
+                i++
+                console.log("Connected node: ", linkedNode.id, linkedNode.data)
+            })
+            setActiveNode(clickedNode)
+        }
     }
     
     //adds the posRef to a list with the key of the node it is the posRef for
@@ -48,7 +54,12 @@ const Main = ({mouse}) => {
         
         //adds all the nodes to the array that gets rendered
         g.forEachNode((node) => {
-            value.push( <NodeObject key={node.id} value={node.id} onClick={onClick} getPosRef={getPosRef} pos={node.data.pos} scene={node.data.scene} mouse={mouse} testing={node.data.testing}/> )
+            if(node.id == 'a'){
+                value.push( <NodeObject key={node.id} value={node.id} onClick={onClick} getPosRef={getPosRef} pos={[0,0,0]} scene={node.data.scene} mouse={mouse} testing={node.data.testing}/> )
+            }
+            else{
+                value.push( <NodeObject key={node.id} value={node.id} onClick={onClick} getPosRef={getPosRef} pos={node.data.pos} scene={node.data.scene} mouse={mouse} testing={node.data.testing}/> )
+            }
         })
 
         return value
