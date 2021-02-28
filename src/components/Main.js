244bf6761c1ce3797e901d7dec600dacd18661ab
Sammy Jackson <sam.jackson@live.ca>
2021-02-28 16:51:17 -0500

    changing position values on click reacts with other nodes

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,9 +12,10 @@ const Main = ({mouse}) => {
     var createGraph = require('ngraph.graph');
     var g = createGraph();
 
-    g.addNode('a', { scene: <JackyObject />, pos: [0,1,1]})
-    g.addNode('b', { scene: <JackyObject />, pos: [0,2,2]})
-    g.addNode('c', { scene: <JackyObject />, pos: [0,3,3]})
+    g.addNode('a', { scene: <JackyObject />, pos: [0,5,1]})
+    g.addNode('b', { scene: <JackyObject />, pos: [0,-5,2]})
+    g.addNode('c', { scene: <JackyObject />, pos: [3,0,3]})
+    g.addNode('d', { scene: <JackyObject />, pos: [-3,0,-3]})
 
     g.addLink('a', 'b')
     g.addLink('a', 'c')
@@ -22,7 +23,7 @@ const Main = ({mouse}) => {
     const onClick = (clickedNode) => {
         g.forEachNode((node) => {
             //set each node to the original position and center the clicked node
-            node.id == clickedNode ? node.data.pos = [0,0,0] : node.data.pos = [0,0,30]
+            node.id == clickedNode ? positions.current.get(node.id).current = [0,0,0] : positions.current.get(node.id).current = node.data.pos
         })
         //change the positions of the linked nodes to be around the clicked node
         g.forEachLinkedNode(clickedNode, (linkedNode) => {
@@ -34,17 +35,6 @@ const Main = ({mouse}) => {
     
     const findPosition = (node, pos) => {
         return null
-    }
-
-    const testClick = (node) => {
-        console.log(node)
-        console.log(positions.current)
-        console.log(positions.current.get('a'))
-        
-        if(node == 'c'){
-            positions.current.get(node).current[1] = -4
-        }
-        setActiveNode(node)
     }
     
     //adds the posRef to a list with the key of the node it is the posRef for
@@ -58,7 +48,7 @@ const Main = ({mouse}) => {
         
         //adds all the nodes to the array that gets rendered
         g.forEachNode((node) => {
-            value.push( <NodeObject key={node.id} value={node.id} onClick={testClick} getPosRef={getPosRef} pos={node.data.pos} scene={node.data.scene} mouse={mouse} testing={node.data.testing}/> )
+            value.push( <NodeObject key={node.id} value={node.id} onClick={onClick} getPosRef={getPosRef} pos={node.data.pos} scene={node.data.scene} mouse={mouse} testing={node.data.testing}/> )
         })
 
         return value
