4bdb17f9ccb605fa80700e8a1557654c9a71ccce
Sammy Jackson <sam.jackson@live.ca>
2021-02-28 11:07:35 -0500

    pushing an array of scenes to canvas

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,23 @@ import JackyObject from './JackyObject'
 const Main = ({mouse}) => {
     let x = mouse.current[0]
 
+    const findPosition = (node, pos) => {
+        return null
+    }
+
+    const testReturn = () => {
+        let value = []
+        for(let i = 1; i < 3; i++){
+            value.push( <NodeObject mouse={mouse} scene={<JackyObject />} position1={[0,i,i]} position2={[i,0,-i]} /> )
+        }
+        return value
+    }
+
     return (
         <Suspense fallback={null}>
-            <NodeObject mouse={mouse} scene={<JackyObject />} position1={[0,5,1]} position2={[0,0,0]} />
-            <NodeObject mouse={mouse} scene={<JackyObject />} position1={[0,0,0]} position2={[0,0,15]} />
+            {/* <NodeObject mouse={mouse} scene={<JackyObject />} position1={[0,5,1]} position2={[0,0,0]} />
+            <NodeObject mouse={mouse} scene={<JackyObject />} position1={[0,0,0]} position2={[0,0,15]} /> */}
+            {testReturn()}
         </Suspense>
     )
 }
